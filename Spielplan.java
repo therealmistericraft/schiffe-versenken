c52655f0309d1fb929938af81c4347acf3a48c77
therealmistericraft <65766422+therealmistericraft@users.noreply.github.com>
2021-05-10 08:40:20 +0200

    Started schiffeAufnehmen()
    ToDo: check if all fields are on spielplan, check if no ships are near by

--- Spielplan.java
+++ Spielplan.java
@@ -8,12 +8,32 @@ public class Spielplan
     // instance variables - replace the example below with your own
     private int[][] fields;
     private int test;
+    private int a;
+    private int b;
+    private int c;
+    private int d;
+    private int e;
+    private int f;
+    private int g;
+    private int h;
+    private int i;
+    private int j;
 
     /**
      * Constructor for objects of class Spielplan
      */
     public Spielplan()
     {
+        a = 1;
+        b = 2;
+        c = 3;
+        d = 4;
+        e = 5;
+        f = 6;
+        g = 7;
+        h = 8;
+        i = 9;
+        j = 10;
         System.out.println("1");
         for (int i = 10; i>0; i--) {
             System.out.println("2");
@@ -44,9 +64,34 @@ public class Spielplan
      * @param   reihe   Reihe, in der das gewuenschte Feld liegt
      * @param   orientierung   nord, ost, sued und west
      */
-    public void schiffeAufnehmen(int spalte, int reihe, String orientierung)
+    public void schiffeAufnehmen(int spalte, int reihe, int groesse, String orientierung)
     {
-           
+        for (int i = 0, i=groesse, i++) {
+            if (orientierung == "nord") {
+                zustand = this.auskunftGeben(spalte, reihe-i);
+                if (zustand == 0) {
+                    fields[spalte][reihe-i] = 1;   
+                }
+            }
+            if (orientierung == "ost") {
+                zustand = this.auskunftGeben(spalte+i, reihe);
+                if (zustand == 0) {
+                    fields[spalte][reihe+1] = 1;   
+                }
+            }
+            if (orientierung == "sued") {
+                zustand = this.auskunftGeben(spalte, reihe+i);
+                if (zustand == 0) {
+                    fields[spalte][reihe+i] = 1;   
+                }
+            }
+            if (orientierung == "west") {
+                zustand = this.auskunftGeben(spalte-i, reihe);
+                if (zustand == 0) {
+                    fields[spalte-1][reihe] = 1;   
+                }
+            }
+        }
     }
     
     /**
