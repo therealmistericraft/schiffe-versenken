6f2226427a85e7fed7718f6cc9c7a51383adc92a
therealmistericraft <65766422+therealmistericraft@users.noreply.github.com>
2021-05-10 08:16:43 +0200

    Update Spielplan.java

--- Spielplan.java
+++ Spielplan.java
@@ -24,4 +24,38 @@ public class Spielplan
             }
         }
     }
+    
+    /**
+     * Gibt zurück, welchen Zustand das Feld hat
+     *
+     * @param   spalte   Spalte, in der das gewuenschte Feld liegt
+     * @param   reihe   Reihe, in der das gewuenschte Feld liegt
+     * @return Wert des jeweiligen Feldes, 0=nichts, 1=schiff, 2=getroffen, 3=versucht
+     */
+    public int auskunftGeben(int spalte, int reihe)
+    {
+           
+    }
+    
+     /**
+     * Setzt ein Schiff auf ein Anfangsfeld mit einer Orientierung
+     *
+     * @param   spalte   Spalte, in der das gewuenschte Feld liegt
+     * @param   reihe   Reihe, in der das gewuenschte Feld liegt
+     * @param   orientierung   nord, ost, sued und west
+     */
+    public void schiffeAufnehmen(int spalte, int reihe, String orientierung)
+    {
+           
+    }
+    
+    /**
+     * Prueft, ob ein Spieler gewonnen hat und gibt ggf. das Spielerobjekt zurueck
+     *
+     * @return Objekt des Spielers, der gewonnen hat
+     */
+    public Spieler gewinner()
+    {
+           
+    }
 }
